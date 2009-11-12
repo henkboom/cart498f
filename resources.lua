d7a48b30b571330563906a2927be6e594cb25e2e
Henk Boom <henk@henk.ca>
2009-11-12 00:38:40 +0000

    Added a bunch of files I forgot.

---
+++ resources.lua
@@ -0,0 +1,19 @@
+local g = require 'dokidoki.graphics'
+local c = require 'constants'
+
+local room_size = {c.room_width, c.room_height}
+
+return {
+  room_backgrounds = {
+    g.sprite_from_image('rooms/1.png', room_size)
+  },
+  pyx_glow = g.sprite_from_image('sprites/pyx_glow.png', {128, 128}, 'center'),
+  darkness_small =
+    g.sprite_from_image('sprites/darkness.png', {48, 48}, 'center'),
+  darkness_big =
+    g.sprite_from_image('sprites/darkness.png', {192, 192}, 'center'),
+  darkness_big =
+    g.sprite_from_image('sprites/darkness.png', {192, 192}, 'center'),
+  debug_box =
+    g.sprite_from_image('sprites/debug_box.png', {1, 1}, 'center'),
+}
