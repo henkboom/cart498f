77e5b937e70d8529c9199c4311aef48da4dabaf2
Henk Boom <henk@henk.ca>
2009-11-23 23:16:52 -0500

    refactoring for cohesion

--- init.lua
+++ init.lua
@@ -1,15 +1,104 @@
 --dokidoki_disable_debug = true
 require 'dokidoki.module' [[]]
 
+require 'glfw'
+
 local actor_scene = require 'dokidoki.actor_scene'
 local kernel = require 'dokidoki.kernel'
+local v2 = require 'dokidoki.v2'
 
+local collision = require 'collision'
 local constants = require 'constants'
+local entities = require 'entities'
 local game = require 'game'
+local level = require 'level'
+local util = require 'util'
+
+import(require 'gl')
+import(require 'glu')
+
+update_phases = {'pre_update', 'update', 'collision_check', 'post_update'}
+draw_phases = {'draw_setup', 'draw_dark', 'draw_glow', 'draw_terrain', 'draw'}
+
+
+function init_mouse_input(game)
+  local center = v2(constants.width/2, constants.height/2)
+
+  local mouse_movement = v2(0, 0)
+  
+  game.add_actor{
+    pre_update = function ()
+      mouse_movement = v2(glfw.GetMousePos()) - center
+      glfw.SetMousePos(center.x, center.y)
+      print(mouse_movement)
+    end
+  }
+end
+
+function init_collision(game)
+  local obstacle_index
+
+  game.add_actor{
+    pre_update = function ()
+      obstacle_index = obstacle_index or
+        util.make_collision_index(game.get_actors_by_tag('obstacle'), 32, 5)
+    end,
+    collision_check = function ()
+      for _, e in ipairs(game.get_actors_by_tag('entity')) do
+        for _, o in ipairs(obstacle_index.lookup(e.pos)) do
+          local correction = collision.collide(e, o)
+          if correction then
+            e.pos = e.pos + correction
+            e.handle_collision(v2.norm(correction))
+          end
+        end
+      end
+      for _, e in ipairs(game.get_actors_by_tag('enemy')) do
+        for _, b in ipairs(game.get_actors_by_tag('player_bullet')) do
+          local correction = collision.collide(e, b)
+          if correction then
+            e.hit()
+            b.hit()
+          end
+        end
+      end
+    end,
+
+    draw_setup = util.gl_setup,
+    draw_glow = function ()
+      glBlendFunc(GL_SRC_ALPHA, GL_ONE)
+    end,
+    draw_terrain = function ()
+      glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+    end,
+  }
+end
+
+function init (game)
+  -- for some dumb reason the random seed gets clobered when we open a window,
+  -- so we have to do it here
+  math.randomseed(os.time())
+
+  game.resources = require 'resources'
+
+  init_mouse_input(game)
+  init_collision(game)
+
+  local player_pos = v2(constants.room_width * 2.5, constants.room_height/2)
+
+  local player_controller = entities.make_player_controller(game)
+  local player = entities.make_player(game, player_controller, player_pos)
+  game.add_actor(player_controller)
+  game.add_actor(player)
+
+  --game.add_actor(entities.make_filler(game, player_pos + v2(100, 100)))
+
+  game.add_actor(util.make_following_camera(game, player))
+
+  level.add_area(game, v2(0, 0), 5, 5)
+end
 
 kernel.set_video_mode(constants.width, constants.height)
 kernel.set_ratio(constants.width/constants.height)
-kernel.start_main_loop(actor_scene.make_actor_scene(
-  {'pre_update', 'update', 'collision_check', 'post_update'},
-  {'draw_setup', 'draw_dark', 'draw_glow', 'draw_terrain', 'draw'},
-  game.init))
+kernel.start_main_loop(
+  actor_scene.make_actor_scene(update_phases, draw_phases, init))
